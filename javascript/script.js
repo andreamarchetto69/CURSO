3fbe3e4d0bc532ca4667dbbc14aee937bc77d1dd
Andrea V. Marchetto <104329346+andreamarchetto69@users.noreply.github.com>
2022-06-23 17:12:25 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+function inclusive() {onclick("Sitio Inclusivo")}
+
+function inclusive() {
+  document.body.style.backgroundColor = "black";
+  document.getElementById("section").style.backgroundColor = "black";
+  document.getElementById("section").style.backgroundColor = "white";
+  document.getElementById("aside").style.backgroundColor = "black";
+  document.getElementById("aside").style.backgroundColor = "white";
+  document.getElementById("card").style.backgroundColor = "black";
+  document.getElementById("card").style.backgroundColor = "white";
+}
+
+function copy() {
+    alert("Autora: Tribiliky Creaciones: Tarjetería estilo gráfica, diseños de logo, artesanías...  EMAIL: amarch20133@gmail.com");
+    }
+
+  
