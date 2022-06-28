d877f536fae61788f669e4a25a7cb920855a132c
Andrea V. Marchetto <104329346+andreamarchetto69@users.noreply.github.com>
2022-06-28 00:37:02 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function cambiarModo() { 
+    var cuerpoweb = document.querySelector('section');
+    cuerpoweb.classList.toggle("oscuro");
+}
+
+function  copy() {
+    alert("Autora: Tribiliky Creaciones: Tarjetería estilo gráfica, diseños de logo, artesanías...  EMAIL: amarch20133@gmail.com");
+}
+
